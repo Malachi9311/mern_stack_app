d8298ea727481f41c0e7d40040bcae729a2493a5
Malachi9311 <ntuthuko93dlamini@gmail.com>
2023-10-02 16:21:12 +0200

    titles

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          InstaGam Clone
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
